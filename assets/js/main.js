03f301c72e077598ea8182c750a921012540f65f
shinonhorror <shinonhorror@gmail.com>
2023-08-11 23:35:23 +0300

    Sync slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,6 @@ $(document).ready(function () {
 		$('body,html').animate({ scrollTop: top }, 1000);
 	});
 
-	$('.footer__about').on('click', 'a', function (event) {
-		event.preventDefault();
-		var id = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('body,html').animate({ scrollTop: top }, 1000);
-	});
-
 	$(function () {
 		$('#phone').mask('+7 (999) 999-9999');
 	});
@@ -33,6 +26,20 @@ $(document).ready(function () {
 			pauseOnHover: false,
 			dots: true,
 			arrows: false,
+			asNavFor: '.text__slider',
+		});
+
+		$('.text__slider').slick({
+			infinite: true,
+			slidesToShow: 1,
+			speed: 1000,
+			autoplay: true,
+			autoplaySpeed: 5000,
+			pauseOnFocus: false,
+			pauseOnHover: false,
+			arrows: false,
+			asNavFor: '.center',
+			fade: true,
 		});
 	});
 
@@ -44,12 +51,6 @@ $(document).ready(function () {
 				item.className === 'slick-active'
 					? item.lastChild.classList.add('active')
 					: item.lastChild.classList.remove('active');
-			});
-
-			document.querySelectorAll('.main__about').forEach((item) => {
-				item.id === `main${currentSlide}`
-					? item.classList.add('active-main')
-					: item.classList.remove('active-main');
 			});
 		},
 	);
